a915010b2b6a6a2783164af16c7c33a0868093f8
kapadokia <ktnyandoch@gmail.com>
2022-10-21 07:36:57 +0300

    successfully solved anagrams issue

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word
+    attr_accessor :matches
+
+    def initialize word
+        @word =word
+    end
+
+    def match matches
+        data = matches.select do |anagram| 
+            sorted = @word.chars.to_a.sort.join.downcase
+            anagram.chars.to_a.sort.join.downcase==sorted
+        end
+
+    end
+end
+
+ang = Anagram.new("aba")
+str = ang.match %w[hello world zombies pants dipper aba]
